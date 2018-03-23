aaf31623a3702d0ccef02bdbe0c40c89520cfde9
seung-yeol <oks153123@naver.com>
2018-03-23 15:54:45 +0900

    전거 업뎃

--- 3주차/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ 3주차/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -5,19 +5,17 @@ import org.junit.Test;
 
 import java.sql.SQLException;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.is; // static 이 붙으면 해당 패키지에 static 메서드를 바로사용가능
-import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDaoTest {
-    private UserDao userDao;
+    private UserDao jejuUserDao;
     private UserDao hallaUserDao;
 
     //before 하면 테스트 실행전에 무조건 실행됨
     @Before
     public void setup(){
-        userDao = new JejuUserDao();
+        jejuUserDao = new JejuUserDao();
         hallaUserDao = new HallaUserDao();
     }
 
@@ -25,7 +23,7 @@ public class UserDaoTest {
     //Exception처리는 확실히 처리할 수 있을때 catch해내어야함.
     public void get() throws SQLException, ClassNotFoundException {
         int id= 1;
-        User user = userDao.get(id);
+        User user = jejuUserDao.get(id);
 
         //assertThat 은 비교하는 용으로 사용
         assertThat(user.getId(), is(1));
@@ -40,9 +38,9 @@ public class UserDaoTest {
         user.setPassword("1234");
 
         //return값 없어도 되지만, 테스트를 하기위해 받아옴
-        Integer id = userDao.insert(user);
+        Integer id = jejuUserDao.insert(user);
 
-        User insertedUser = userDao.get(id);
+        User insertedUser = jejuUserDao.get(id);
         assertThat(insertedUser.getId(), is(id));
         assertThat(insertedUser.getName(), is(user.getName()));
         assertThat(insertedUser.getPassword(), is(user.getPassword()));

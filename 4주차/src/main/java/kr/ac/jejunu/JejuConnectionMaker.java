81a22f471c1b3707ddffc94310ea8ba1cb5c2c46
seung-yeol <oks153123@naver.com>
2018-04-06 16:45:00 +0900

    환경변수 설정

--- 4주차/src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
+++ 4주차/src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
@@ -1,15 +1,31 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JejuConnectionMaker implements ConnectionMaker {
+
+    //디버그화살표 옆에 스피너 누르고 edit configuration 누르고
+    //envirenment variable 누르고 밑에 밸류 만듬
+    //대문자를 소문자로 인식하고 _를 .으로 인식함
+    //ex) DB_CLASSNAME 이라고 만듬
+    @Value("${db.className}")
+    private String className;
+    @Value("{db.url}")
+    private String url;
+    @Value("{db.username}")
+    private String userName;
+    @Value("{db.password}")
+    private String password;
+
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        className = "com.mysql.jdbc.Driver";
+        Class.forName(className);
 
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8"
-                , "root", "1234");
+        return DriverManager.getConnection(url, userName, password);
     }
 }

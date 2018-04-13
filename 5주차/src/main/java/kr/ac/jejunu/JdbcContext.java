1260e5f17008cd6bf0c5756a07ecf729a7951816
seung-yeol <oks153123@naver.com>
2018-04-13 16:37:42 +0900

    JdbcContext로 뺏어요

---
+++ 5주차/src/main/java/kr/ac/jejunu/JdbcContext.java
@@ -0,0 +1,119 @@
+package kr.ac.jejunu;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    public User jdbcContextForGet(StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } finally {
+            if (resultSet != null)
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
+        return user;
+    }
+    public void jdbcContextForUpdate(StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+
+            preparedStatement.executeUpdate();
+
+        } finally {
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+        }
+    }
+    public Integer jdbcContextForInsert(StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Integer id;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+
+            preparedStatement.executeUpdate();
+
+//            preparedStatement = connection.prepareStatement("select last_insert_id()");
+            //Statement.RETURN_GENERATED_KEYS를 InsertUserStatementStrategy에 매개변수로 추가로 넣어서 getGeneratedKeys()로 받아옴
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+
+            id = resultSet.getInt(1);
+        } finally {
+            if (resultSet != null)
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+        }
+        return id;
+    }
+}
--- 5주차/src/main/java/kr/ac/jejunu/UserDao.java
+++ 5주차/src/main/java/kr/ac/jejunu/UserDao.java
@@ -4,146 +4,30 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
+    private final JdbcContext jdbcContext;
 //    private final ConnectionMaker connectionMaker; 얘를 밑에걸로 바꿔줌
-    private final DataSource dataSource;
 
-    public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
     public User get(int id) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        User user = null;
-        try {
-            connection = dataSource.getConnection();
-            StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
-            preparedStatement = statementStrategy.makeStatement(connection);
-
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()) {
-                user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-        } finally {
-            if (resultSet != null)
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (preparedStatement != null)
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-        }
-        return user;
+        StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
+        return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
     public Integer insert(User user) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        Integer id;
-        try {
-            connection = dataSource.getConnection();
-            StatementStrategy statementStrategy = new InsertUserStatementStrategy(user);
-            preparedStatement = statementStrategy.makeStatement(connection);
-
-            preparedStatement.executeUpdate();
-
-//            preparedStatement = connection.prepareStatement("select last_insert_id()");
-            //Statement.RETURN_GENERATED_KEYS를 InsertUserStatementStrategy에 매개변수로 추가로 넣어서 getGeneratedKeys()로 받아옴
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-
-            id = resultSet.getInt(1);
-        } finally {
-            if (resultSet != null)
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (preparedStatement != null)
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-        }
-        return id;
+        StatementStrategy statementStrategy = new InsertUserStatementStrategy(user);
+        return jdbcContext.jdbcContextForInsert(statementStrategy);
     }
 
     public void update(User user) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            StatementStrategy statementStrategy = new UpdateUserStatementStrategy(user);
-            preparedStatement = statementStrategy.makeStatement(connection);
-
-            preparedStatement.executeUpdate();
-
-        } finally {
-            if (preparedStatement != null)
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-        }
+        StatementStrategy statementStrategy = new UpdateUserStatementStrategy(user);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Integer id) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
-            preparedStatement = statementStrategy.makeStatement(connection);
-
-            preparedStatement.executeUpdate();
-
-        } finally {
-            if (preparedStatement != null)
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-        }
+        StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 }
